9632a1a43a976909b7de0bec4922f4383ac34607
Josdegar <josdegarfs@gmail.com>
2024-12-03 01:33:59 -0300

    Mandando o projeto(Incompleto)

---
+++ src/components/cart/sidebar.tsx
@@ -0,0 +1,60 @@
+"use client"
+
+import { RocketIcon } from "lucide-react"
+import { Button } from "../ui/button"
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "../ui/sheet"
+import { Separator } from "../ui/separator"
+import { useCartStore } from "@/stores/cart-store"
+import { CartItem } from "./item"
+
+export const CartSidebar = () => {
+
+    // zustand
+    const { cart } = useCartStore(state => state)
+
+    let subtotal = 0 
+    for(let item of cart) {
+        subtotal += item.quantity * item.product.price
+    }
+
+/* O Trigger é uma área clicavél e o botão também é. Isso gera um conflito. Então é necessário colocar o asChild para anular a área clicavél do Trigger e deixar só a do button mas com a mesma funcionalidade */
+    return(
+        <Sheet >
+            <SheetTrigger asChild>
+                <Button className="relative">
+                    <RocketIcon className="mr-2"/>
+                    <p>Carrinho</p>
+                    {cart.length > 0 &&
+                        <div className="size-3 absolute bg-red-600 rounded-full -right-1 -top-1"></div>
+                    }
+                </Button>
+            </SheetTrigger>
+            <SheetContent>
+                <SheetHeader>
+                    <SheetTitle>Carrinho</SheetTitle>
+                </SheetHeader>
+                
+                <div className="flex flex-col gap-5 my-3">
+                    {cart.map(item => (
+                        <CartItem key={item.product.id} item={item}/>
+                    ))}
+                </div>
+
+                <Separator className="my-4"/>
+
+                <div className="flex justify-between items-center text-xs">
+                    <div>Subtotal: </div>
+                    <div>R$ {subtotal.toFixed(2)}</div>
+                </div>
+
+                <Separator className="my-4"/>
+
+                <div className="text-center">
+                    <Button 
+                        disabled={cart.length === 0}
+                    >Finalizar Compra</Button>
+                </div>
+            </SheetContent>
+        </Sheet>
+    )
+}
---
+++ src/components/footer.tsx
@@ -0,0 +1,12 @@
+import { Separator } from "./ui/separator"
+
+export const Footer = () => {
+    return(
+        <footer className="mt-5">
+            <Separator/>
+            <div className="my-5 text-center opacity-50">
+                Criado pela B7Web
+            </div>
+        </footer>
+    )
+}
